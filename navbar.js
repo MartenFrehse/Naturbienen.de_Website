abab532197c042238ae44e87092daa6dd5e55fac
MartenFrehse <martenfrehse11@gmail.com>
2023-02-25 17:24:25 +0100

    topnavbar_fonts
    Adding TopNavBar for index and burgerfont for navbar + js

---
+++ navbar.js
@@ -0,0 +1,21 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("MyTopnav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
